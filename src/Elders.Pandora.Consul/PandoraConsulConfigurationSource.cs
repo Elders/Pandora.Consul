632ecdf9295112e52edfa5492f8423d698d4e1ac
AlexPlom <alexiosplomaritis@gmail.com>
2018-10-01 15:25:39 +0300

    Adds PandoraConsulConfigurationSource

---
+++ src/Elders.Pandora.Consul/PandoraConsulConfigurationSource.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace Elders.Pandora
+{
+    public class PandoraConsulConfigurationSource : IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            string applicationName = Environment.GetEnvironmentVariable("APPLICATION_NAME");
+            IPandoraContext context = new ClusterContext(applicationName);
+            IConfigurationRepository repository = new ConsulForPandora(new Uri("http://consul.local.com:8500"));
+            Pandora pandora = new Pandora(context, repository);
+
+            return new PandoraConfigurationProvider(pandora);
+        }
+    }
+}
