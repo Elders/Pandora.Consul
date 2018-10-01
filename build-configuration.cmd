f2a0371643cd536405a47503f00ce87056abd9d1
AlexPlom <alexiosplomaritis@gmail.com>
2018-10-01 12:37:41 +0300

    Updates Packages and targets .netcore 2.1

--- build-configuration.cmd
+++ build-configuration.cmd
@@ -1,11 +1,3 @@
 @echo off
 
-%FAKE% %NYX% "target=clean" -st
-%FAKE% %NYX% "target=RestoreNugetPackages" -st
-
-IF NOT [%1]==[] (set RELEASE_NUGETKEY="%1")
-
-SET SUMMARY="Pandora aims to externalize the application configuration."
-SET DESCRIPTION="Pandora aims to externalize the application configuration."
-
-%FAKE% %NYX% appName=Elders.Pandora.Consul appSummary=%SUMMARY% appDescription=%DESCRIPTION% nugetPackageName=Pandora.Consul nugetkey=%RELEASE_NUGETKEY%
+@powershell -File .nyx\build.ps1 '--appname=Pandora.Consul' '--nugetPackageName=Pandora.Consul'
