2ea5ca1f57aafbac3e050685a311926a7a31f2c5
mynkow <mynkow@gmail.com>
2018-11-13 16:19:31 +0200

    Fixes PandoraConsulConfigurationSource

--- src/Elders.Pandora.Consul/PandoraConsulConfigurationSource.cs
+++ src/Elders.Pandora.Consul/PandoraConsulConfigurationSource.cs
@@ -7,8 +7,7 @@ namespace Elders.Pandora
     {
         public IConfigurationProvider Build(IConfigurationBuilder builder)
         {
-            string applicationName = Environment.GetEnvironmentVariable("APPLICATION_NAME");
-            IPandoraContext context = new ClusterContext(applicationName);
+            IPandoraContext context = new ApplicationContext();
             IConfigurationRepository repository = new ConsulForPandora(new Uri("http://consul.local.com:8500"));
             Pandora pandora = new Pandora(context, repository);
 
